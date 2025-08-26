2976a7877162dde38912ca798f393d027f032097
F4t4l_3rr0r <180484874+FatalError-CS@users.noreply.github.com>
2025-08-26 11:41:54 -0300

    Update SingUp.py

--- SingUp.py
+++ SingUp.py
@@ -3,85 +3,75 @@ import time
 import sys
 import json
 from datetime import datetime
-
-def ler_valor_nao_vazio(usuário_variavel):
-    valor_lido = input(f'\nCrie um nome de {usuário_variavel}: ')
-    while valor_lido == '':
-        print(f'\nO nome de {usuário_variavel} não pode ser vazio!')
-        valor_lido = input(f'Crie um nome de {usuário_variavel}: ')
-    return valor_lido
-
-def ler_valor_de_senha(senha_variavel):
-    valor_lido = input(f'\nCrie uma {senha_variavel} para o usuário: ')
-    while valor_lido == '':
-        print(f'\nA {senha_variavel} não pode ser vazia!')
-        valor_lido = input(f'Crie uma {senha_variavel} para o usuário: ')
-    return valor_lido
-
-def verifica_data_valida(data_texto):
+def  read_non_empty_value(user_variable):
+    read_value = input(f'\nCreate a {user_variable} name: ')
+    while read_value == '':
+        print(f'\nThe name of {user_variable} cannot be empty!')
+        read_value = input(f'Create a {user_variable} name: ')
+    return read_value
+def  read_password_value(password_variavel):
+    read_value = input(f'\nCreate a {password_variable} for the user: ')
+    while read_value == '':
+        print(f'\n{password_variable} cannot be empty!')
+        read_value = input(f'Create a {password_variable} for the user: ')
+    return read_value
+def check_valid_birthday(birthday_text):
     try:
-        data_valida = datetime.strptime(data_texto, "%d/%m/%Y")
+        valid_birthday = datetime.strptime(birthday_text, "%d/%m/%Y")
         return True
     except:
         return False
-
-def ler_data_valida():
-    dataNascimentoString = input('\nEntre com a data de nascimento: ')
-    while not verifica_data_valida(dataNascimentoString):
-        print('\nData inválida!')
-        dataNascimentoString = input('Entre com a data de nascimento: ')
-    dataNascimento = datetime.strptime(dataNascimentoString, "%d/%m/%Y")
+def read_valid-birthday():
+    birthdayString = input('\nEnter your birthday: ')
+    while not check_valid_birthday(birthdayString):
+        print('\nInvalid!')
+        birthdayString = input('Enter your birthday: ')
+    birthday = datetime.strptime(birthdayString, "%d/%m/%Y")
     print()
-    return dataNascimento
-
-def carregar():
-    pontos = ['.', '.', '.']
-    for ponto in pontos:
-        sys.stdout.write(ponto)
+    return birthday
+def loading():
+    dots = ['.', '.', '.']
+    for dot in dots:
+        sys.stdout.write(dot)
         sys.stdout.flush()
         time.sleep(1)
-
-def ler_Conta(lista_usuarios):
+def read_Account( user_list):
     while True:
-        usuário = ler_valor_nao_vazio('usuário')
-        if verificar_usuario_existe(usuário, lista_usuarios):
-            print("\nErro: Este nome de usuário já está em uso. Por favor, escolha outro.\n")
+        user =  read_non_empty_value('user')
+        if check_user_exists(user,  user_list):
+            print("\nErro: This username is already in use. Please choose another one..\n")
         else:
             break
-    senha = ler_valor_de_senha('senha')
-    dataNascimento = ler_data_valida()
-    Conta = {
-        'usuário': usuário,
-        'senha': senha,
-        'dataNascimento': dataNascimento.strftime('%d/%m/%Y'),
+    password =  read_password_value('password')
+    birthday = read_valid-birthday()
+    account = {
+        'user': user,
+        'password': password,
+        'birthday': birthday.strftime('%d/%m/%Y'),
     }
     print('______________________________')
-    print('\nConta criada!\n\nRedirecionando', end='')
-    carregar()
-    return Conta
-
-def verificar_usuario_existe(usuario, lista_usuarios):
-    for conta in lista_usuarios:
-        if conta['usuário'] == usuario:
+    print('\naccount criada!\n\nRedirecting', end='')
+    loading()
+    return account
+def check_user_exists(user,  user_list):
+    for account in  user_list:
+        if account['user'] == user:
             return True
     return False
 
+def read_json():
+    with open("accounts.json", "r") as my_file:
+        users_list = json.load(my_file)
+    return users_list
 
-def ler_json():
-    with open("contas.json", "r") as meu_arquivo:
-        lista_usuários = json.load(meu_arquivo)
-    return lista_usuários
-
-def salvar_json(lista):
-    with open("contas.json", "w") as meu_arquivo:
-        lista_json = json.dumps(lista_usuários, indent=4)
-        meu_arquivo.write(lista_json)
-
-lista_usuários = ler_json()
-
+def  save_json(list):
+    with open("accounts.json", "w") as my_file:
+        list_json = json.dumps(users_list, indent=4)
+        my_file.write(list_json)
+users_list = read_json()
 for i in range(1):
-    meu_usuário = ler_Conta(lista_usuários)
-    lista_usuários.append(meu_usuário)
-    salvar_json(lista_usuários)
-    subprocess.run(["python", "MenuPrincipal.py"])
+    my_user = read_Account(users_list)
+    users_list.append(my_user)
+     save_json(users_list)
+    subprocess.run(["python", "MainMenu.py"])
     exit()
